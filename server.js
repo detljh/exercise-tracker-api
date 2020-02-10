f916b00858c0850cae9276aa5fb1edf774cb8cc7
lisa <lisa.ng8@hotmail.com>
2020-02-10 17:07:51 +1100

    logs

--- server.js
+++ server.js
@@ -61,7 +61,7 @@ app.use((err, req, res, next) => {
 
 app.route('/api/exercise/new-user')
   .post((req, res) => {
-    let name = req.body.username
+    let name = req.body.username;
     User.findOne({username: name}, (err, user) => {
       if (user) return res.send('Username already taken!');
 
@@ -70,7 +70,7 @@ app.route('/api/exercise/new-user')
         if (err) return console.log(err);
 
         res.json({
-          userId: user._id,
+          user_id: user._id,
           username: user.username
         });
       });
@@ -112,12 +112,50 @@ app.route('/api/exercise/add')
 
 app.route('/api/exercise/users')
   .get((req, res) => {
-
+    User.find((err, users) => {
+      if (err) return console.log(err);
+      res.send(users);
+    })
   });
 
 app.route('/api/exercise/log')
   .get((req, res) => {
+    let id = req.query.userId;
+    let from = req.query.from;
+    let to = req.query.to;
+    let limit = req.query.limit === '' ? 0 : Math.abs(Number(req.query.limit));
 
+    User.findOne({_id: id}, {__v: 0}, (err, user) => {
+      if (user) {       
+        let query = {user_id: user._id}; 
+        if (from) {
+          query = Object.assign({}, query, {'date': {'$gte': from}});  
+        }
+
+        if (to) {
+          query = Object.assign({}, query, {'date': {'$lte': to}});
+        }
+
+        Exercise.find(query, {_id: 0, __v: 0}).limit(limit).exec((err, logs) => {
+          if (logs) {
+            logs = logs.map(log => {
+              return {
+                description: log.description,
+                duration: log.duration,
+                date: log.date.toDateString()
+              };
+            });
+            res.json({
+              ...user.toObject(),
+              count: logs.length,
+              log: logs
+            });
+          }
+        });
+      } else {
+        return res.send('Unknown user id');
+      }
+    });
   });
 
 const listener = app.listen(process.env.PORT || 3000, () => {
